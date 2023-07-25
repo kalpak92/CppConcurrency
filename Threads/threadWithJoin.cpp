17078fb822e866fff3d60b8b23d0de6b5a9afd54
kalpak92 <kals9seals@gmail.com>
2023-07-24 23:51:07 -0700

    thread methods

---
+++ Threads/threadWithJoin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+
+int main()
+{
+
+  std::thread t([]
+    {
+        std::cout << std::this_thread::get_id() << std::endl;
+        });
+
+  t.join(); 
+
+}
